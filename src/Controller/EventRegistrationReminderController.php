25550da9dfb463af68c4ccb6758567e0c4e190f3
Marko Cupic <m.cupic@gmx.ch>
2022-04-25 22:45:25 +0200

    Add the suffix in the title field only if the tour guide has recently received a reminder

--- src/Controller/EventRegistrationReminderController.php
+++ src/Controller/EventRegistrationReminderController.php
@@ -17,7 +17,7 @@ namespace Markocupic\SacEventRegistrationReminder\Controller;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Monolog\ContaoContext;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Exception as DbalException;
 use Markocupic\SacEventRegistrationReminder\Data\Data;
 use Markocupic\SacEventRegistrationReminder\Data\DataCollector;
 use Markocupic\SacEventRegistrationReminder\Notification\NotificationGenerator;
@@ -30,6 +30,9 @@ use Safe\Exceptions\StringsException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 /**
  * @Route("/_event_registration_reminder/{sid}", name=EventRegistrationReminderController::class, methods={"GET"}, defaults={"_locale":"%sac_evt_reg_reminder.default_locale%"})
@@ -64,8 +67,11 @@ class EventRegistrationReminderController extends AbstractController
     }
 
     /**
-     * @throws Exception
+     * @throws DbalException
      * @throws StringsException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function __invoke(string $sid): Response
     {
@@ -77,8 +83,11 @@ class EventRegistrationReminderController extends AbstractController
     }
 
     /**
-     * @throws Exception
+     * @throws DbalException
      * @throws StringsException
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function run(): Response
     {
@@ -102,7 +111,8 @@ class EventRegistrationReminderController extends AbstractController
         while ($itCalendar->valid()) {
             $calendarId = (int) $itCalendar->key();
             $arrUsers = $itCalendar->current();
-
+            // Get the reminder interval in days
+            $reminderIntervalD = (int) $this->connection->fetchOne('SELECT sendReminderEach FROM tl_calendar WHERE id = ?', [$calendarId]);
             // Process each backend user
             if (\is_array($arrUsers)) {
                 $itUsers = (new Data($arrUsers))->getIterator();
@@ -138,15 +148,21 @@ class EventRegistrationReminderController extends AbstractController
                             $userName = $this->connection->fetchOne('SELECT name FROM tl_user WHERE id = ?', [$userId]);
 
                             // Get the previous reminder added-on timestamp, if there is one
-                            $prevReminderTstamp = $this->connection->fetchOne('SELECT addedOn FROM tl_event_registration_reminder_notification WHERE user = ? AND calendar = ?', [$userId, $calendarId]);
+                            $prevReminderTstamp = (int) $this->connection->fetchOne(
+                                'SELECT addedOn FROM tl_event_registration_reminder_notification WHERE user = ? AND calendar = ?',
+                                [$userId, $calendarId]
+                            );
 
-                            $prevReminderTstamp = !empty($prevReminderTstamp) ? (int) $prevReminderTstamp : 0;
+                            // Add a suffix to the title to point out lazy instructors/tour guides ;-)
+                            $blnAddSuffix = $prevReminderTstamp && ($prevReminderTstamp + 2 * $reminderIntervalD * 86400) > $this->stopwatch->getRequestTime();
+                            $strSuffix = $blnAddSuffix ? sprintf(' (last time %s)', date('d.m.Y', $prevReminderTstamp)) : '';
+                            $strTitle = sprintf('Sent a reminder to %s%s.', $userName, $strSuffix);
 
                             $set = [
                                 'tstamp' => $this->stopwatch->getRequestTime(),
                                 'addedOn' => $this->stopwatch->getRequestTime(),
-                                'prevReminderTstamp' => !empty($prevReminderTstamp) ? $prevReminderTstamp : 0,
-                                'title' => 'Sent a reminder to '.$userName.($prevReminderTstamp ? ' (since '.date('d.m.Y', $prevReminderTstamp).').' : '.'),
+                                'prevReminderTstamp' => $prevReminderTstamp,
+                                'title' => $strTitle,
                                 'user' => $userId,
                                 'calendar' => $calendarId,
                             ];
@@ -159,7 +175,7 @@ class EventRegistrationReminderController extends AbstractController
                             if ($affectedRows) {
                                 $lastInsertId = $this->connection->lastInsertId();
 
-                                // Delete old records
+                                // Delete the old record
                                 $this->connection->executeStatement(
                                     'DELETE FROM tl_event_registration_reminder_notification WHERE id != ? AND user = ? AND calendar = ?',
                                     [$lastInsertId, $userId, $calendarId],
@@ -184,7 +200,7 @@ class EventRegistrationReminderController extends AbstractController
     }
 
     /**
-     * @throws Exception
+     * @throws DbalException
      */
     private function getNotification(int $calendarId): ?Notification
     {
