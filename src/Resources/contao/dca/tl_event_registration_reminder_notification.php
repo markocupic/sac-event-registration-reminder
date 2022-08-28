d92ae83daf517b2c58b4d01d3f3c94bac395c325
Marko Cupic <m.cupic@gmx.ch>
2022-08-28 23:12:44 +0200

    Fix undefinded array key issues

--- src/Resources/contao/dca/tl_event_registration_reminder_notification.php
+++ src/Resources/contao/dca/tl_event_registration_reminder_notification.php
@@ -58,7 +58,7 @@ $GLOBALS['TL_DCA']['tl_event_registration_reminder_notification'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_event_registration_reminder_notification']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'   => [
                 'label'      => &$GLOBALS['TL_LANG']['tl_event_registration_reminder_notification']['show'],
